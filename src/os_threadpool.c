244aa4afce36bd092b17d66ca4be12b34bd9d623
Radu-Gabriel MILEA <radulce@Radu>
2023-12-11 23:01:23 +0200

    test push

--- src/os_threadpool.c
+++ src/os_threadpool.c
@@ -1,25 +1,27 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-#include "os_threadpool.h"
 #include "log/log.h"
+#include "os_graph.h"
+#include "os_threadpool.h"
 #include "utils.h"
 
 /* Create a task that would be executed by a thread. */
-os_task_t *create_task(void (*action)(void *), void *arg, void (*destroy_arg)(void *))
+os_task_t *create_task(void (*action)(void *), void *arg,
+						void (*destroy_arg)(void *))
 {
 	os_task_t *t;
 
 	t = malloc(sizeof(*t));
 	DIE(t == NULL, "malloc");
 
-	t->action = action;		// the function
-	t->argument = arg;		// arguments for the function
-	t->destroy_arg = destroy_arg;	// destroy argument function
+	t->action = action;           // the function
+	t->argument = arg;            // arguments for the function
+	t->destroy_arg = destroy_arg; // destroy argument function
 
 	return t;
 }
@@ -38,17 +40,18 @@ void enqueue_task(os_threadpool_t *tp, os_task_t *t)
 	assert(tp != NULL);
 	assert(t != NULL);
 
-	/* TODO: Enqueue task to the shared task queue. Use synchronization. */
+	pthread_mutex_lock(&tp->mutex);
+	list_add_tail(&tp->head, &t->list);
+	tp->tasks_enqueued = 1;
+	pthread_cond_signal(&tp->cond);
+	pthread_mutex_unlock(&tp->mutex);
 }
 
 /*
  * Check if queue is empty.
  * This function should be called in a synchronized manner.
  */
-static int queue_is_empty(os_threadpool_t *tp)
-{
-	return list_empty(&tp->head);
-}
+static int queue_is_empty(os_threadpool_t *tp) { return list_empty(&tp->head); }
 
 /*
  * Get a task from threadpool task queue.
@@ -59,16 +62,39 @@ static int queue_is_empty(os_threadpool_t *tp)
 
 os_task_t *dequeue_task(os_threadpool_t *tp)
 {
-	os_task_t *t;
+	os_task_t *t = NULL;
 
-	/* TODO: Dequeue task from the shared task queue. Use synchronization. */
-	return NULL;
+	pthread_mutex_lock(&tp->mutex);
+	while (queue_is_empty(tp) && !tp->work_completed) {
+		tp->num_threads_waiting++;
+		if (tp->num_threads_waiting == tp->num_threads && tp->tasks_enqueued) {
+			pthread_cond_broadcast(&tp->cond);
+			tp->work_completed = 1;
+			pthread_mutex_unlock(&tp->mutex);
+
+			return NULL;
+		}
+		pthread_cond_wait(&tp->cond, &tp->mutex);
+		if (tp->work_completed) {
+			pthread_mutex_unlock(&tp->mutex);
+			return NULL;
+		}
+		tp->num_threads_waiting--;
+	}
+
+	if (!tp->work_completed) {
+		t = list_entry(tp->head.next, os_task_t, list);
+		list_del(&t->list);
+	}
+
+	pthread_mutex_unlock(&tp->mutex);
+	return t;
 }
 
 /* Loop function for threads */
 static void *thread_loop_function(void *arg)
 {
-	os_threadpool_t *tp = (os_threadpool_t *) arg;
+	os_threadpool_t *tp = (os_threadpool_t *)arg;
 
 	while (1) {
 		os_task_t *t;
@@ -79,15 +105,12 @@ static void *thread_loop_function(void *arg)
 		t->action(t->argument);
 		destroy_task(t);
 	}
-
 	return NULL;
 }
 
 /* Wait completion of all threads. This is to be called by the main thread. */
 void wait_for_completion(os_threadpool_t *tp)
 {
-	/* TODO: Wait for all worker threads. Use synchronization. */
-
 	/* Join all worker threads. */
 	for (unsigned int i = 0; i < tp->num_threads; i++)
 		pthread_join(tp->threads[i], NULL);
@@ -104,13 +127,22 @@ os_threadpool_t *create_threadpool(unsigned int num_threads)
 
 	list_init(&tp->head);
 
-	/* TODO: Initialize synchronization data. */
+	rc = pthread_mutex_init(&tp->mutex, NULL);
+	DIE(rc != 0, "pthread_mutex_init");
+
+	rc = pthread_cond_init(&tp->cond, NULL);
+	DIE(rc != 0, "pthread_cond_init");
+
+	tp->num_threads_waiting = 0;
+	tp->work_completed = 0;
+	tp->tasks_enqueued = 0;
 
 	tp->num_threads = num_threads;
 	tp->threads = malloc(num_threads * sizeof(*tp->threads));
 	DIE(tp->threads == NULL, "malloc");
 	for (unsigned int i = 0; i < num_threads; ++i) {
-		rc = pthread_create(&tp->threads[i], NULL, &thread_loop_function, (void *) tp);
+		rc = pthread_create(&tp->threads[i], NULL, &thread_loop_function,
+							(void *)tp);
 		DIE(rc < 0, "pthread_create");
 	}
 
@@ -123,6 +155,8 @@ void destroy_threadpool(os_threadpool_t *tp)
 	os_list_node_t *n, *p;
 
 	/* TODO: Cleanup synchronization data. */
+	pthread_mutex_destroy(&tp->mutex);
+	pthread_cond_destroy(&tp->cond);
 
 	list_for_each_safe(n, p, &tp->head) {
 		list_del(n);
--- src/parallel.c
+++ src/parallel.c
@@ -2,33 +2,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
+#include "log/log.h"
 #include "os_graph.h"
 #include "os_threadpool.h"
-#include "log/log.h"
 #include "utils.h"
 
-#define NUM_THREADS		4
+#define NUM_THREADS 4
 
 static int sum;
 static os_graph_t *graph;
 static os_threadpool_t *tp;
-/* TODO: Define graph synchronization mechanisms. */
+/* Define graph synchronization mechanisms. */
+static pthread_mutex_t graph_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t graph_condition = PTHREAD_COND_INITIALIZER;
 
-/* TODO: Define graph task argument. */
-
-static void process_node(unsigned int idx)
+static void process_node(void *argument)
 {
 	/* TODO: Implement thread-pool based processing of graph. */
+	os_node_t *node;
+
+	pthread_mutex_lock(&graph_mutex);
+	graph_task_arg_t *arg = (graph_task_arg_t *)argument;
+
+	node = graph->nodes[arg->node_index];
+	sum += node->info;
+	graph->visited[arg->node_index] = DONE;
+	pthread_mutex_unlock(&graph_mutex);
+	for (unsigned int i = 0; i < node->num_neighbours; i++) {
+		pthread_mutex_lock(&queue_mutex);
+		if (graph->visited[node->neighbours[i]] == NOT_VISITED) {
+			graph_task_arg_t *new_arg = malloc(sizeof(graph_task_arg_t));
+
+			new_arg->node_index = node->neighbours[i];
+			os_task_t *t = create_task(process_node, (void *)new_arg, free);
+
+			graph->visited[new_arg->node_index] = DONE;
+			enqueue_task(tp, t);
+		}
+		pthread_mutex_unlock(&queue_mutex);
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	FILE *input_file;
+	os_task_t *t;
+	graph_task_arg_t *arg = malloc(sizeof(graph_task_arg_t));
 
+	arg->node_index = 0;
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s input_file\n", argv[0]);
 		exit(EXIT_FAILURE);
@@ -38,14 +64,20 @@ int main(int argc, char *argv[])
 	DIE(input_file == NULL, "fopen");
 
 	graph = create_graph_from_file(input_file);
+	for (unsigned int i = 0; i < graph->num_nodes; i++)
+		graph->visited[i] = NOT_VISITED;
+	pthread_mutex_init(&graph_mutex, NULL);
+	pthread_cond_init(&graph_condition, NULL);
 
-	/* TODO: Initialize graph synchronization mechanisms. */
+	t = create_task(process_node, (void *)arg, free);
 	tp = create_threadpool(NUM_THREADS);
-	process_node(0);
+	enqueue_task(tp, t);
 	wait_for_completion(tp);
 	destroy_threadpool(tp);
 
 	printf("%d", sum);
+	pthread_mutex_destroy(&graph_mutex);
+	pthread_cond_destroy(&graph_condition);
 
 	return 0;
 }
